89398bd302fffe8ff6191c122af0160771a58cda
DavidBrobst <david.brobst@gmail.com>
2023-09-02 00:00:33 -0400

    Initial commit

---
+++ src/Controller/AligetteMainController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/AligetteMainController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AligetteMainController
+{
+    #[Route('/lucky/number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
